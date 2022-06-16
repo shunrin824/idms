a7a4882ecd6c1234783aec28167b51905e6ed4fe
shunrin824 <shunrin@mail.shunrin.com>
2022-06-16 20:56:27 +0900

    バグ修正

--- idms/csv.php
+++ idms/csv.php
@@ -1,46 +1,66 @@
 ﻿<?php
 ini_set('display_errors', "on");
 $s = microtime(true);
-function h($str) {
+function h($str)
+{
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 $id = $_POST['id'];
-$memo = str_replace("\r\n", '', nl2br(h($memo), false));
-while('1' == '1'){
-    if(!file_exists('access')){
+if (isset($memo)) {
+    $memo = str_replace("\r\n", '', nl2br(h($memo), false));
+}
+while ('1' == '1') {
+    if (!file_exists('access')) {
         break;
     }
     sleep("0.1");
 }
 file_put_contents('access', 'writing');
 $datas = json_decode(mb_convert_encoding(file_get_contents('data.json'), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'), 'true');
-foreach($datas as $data){
-    if($_GET['mode'] == "tagrm"){
-        if($data['id'] == $_GET['id']){
-            $tags = $data['tag'];
-            unset($data['tag']);
-            foreach($tags as $tag){
-                if($tag !== $_GET['tag']){
+foreach ($datas as $data) {
+
+    if (isset($_GET['mode'])) {
+        if ($_GET['mode'] == "tagrm") {
+            if ($data['id'] == $_GET['id']) {
+                $tags = $data['tag'];
+                unset($data['tag']);
+                foreach ($tags as $tag) {
+                    if ($tag !== $_GET['tag']) {
+                        $data['tag'][] = $tag;
+                    }
+                }
+                $edata[] = $data;
+            } else {
+                $edata[] = $data;
+            }
+            unset($data);
+            unset($tag);
+            unset($memo);
+        } else {
+            if ($data['id'] == $id) {
+                $tags = explode('、', $_POST['tag']);
+                foreach ($tags as $tag) {
                     $data['tag'][] = $tag;
                 }
+                $data['memo'] = str_replace("\r\n", '', nl2br(h($_POST['memo']), false));
+                $edata[] = $data;
+            } else {
+                $edata[] = $data;
             }
-            $edata[] = $data;
-        }else{
-            $edata[] = $data;
+            unset($data);
+            unset($tag);
+            unset($memo);
         }
-        unset($data);
-        unset($tag);
-        unset($memo);
-    }else{
-        if($data['id'] == $id){
+    } else {
+        if ($data['id'] == $id) {
             $tags = explode('、', $_POST['tag']);
-            foreach($tags as $tag){
+            foreach ($tags as $tag) {
                 $data['tag'][] = $tag;
             }
             $data['memo'] = str_replace("\r\n", '', nl2br(h($_POST['memo']), false));
             $edata[] = $data;
-        }else{
+        } else {
             $edata[] = $data;
         }
         unset($data);
@@ -49,13 +69,14 @@ foreach($datas as $data){
     }
 }
 $s1 = microtime(true);
-$json = json_encode($edata, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-$bytes = file_put_contents("data.json", $json); 
+$json = json_encode($edata, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+$bytes = file_put_contents("data.json", $json);
 $s2 = microtime(true);
 unlink('access');
-echo("write".($s2 - $s1)."<br>");
-echo("all".($s2 - $s)."<br>");
+echo ("write" . ($s2 - $s1) . "<br>");
+echo ("all" . ($s2 - $s) . "<br>");
 ?>
 <html>
- <meta http-equiv="refresh" content="1 ; URL=<?=($_SERVER["HTTP_REFERER"])?>">
+<meta http-equiv="refresh" content="1 ; URL=<?= ($_SERVER["HTTP_REFERER"]) ?>">
+
 </html>
