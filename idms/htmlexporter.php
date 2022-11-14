34f5ae49143b1a959462a9ebba9ecee170fc5b0f
shunrin824 <shunrin@mail.shunrin.com>
2022-11-14 23:00:34 +0900

    途中_htmlエクスポート機能実装中

--- idms/htmlexporter.php
+++ idms/htmlexporter.php
@@ -1,4 +1,6 @@
 <?php
+$idms_path = "/var/www/html/idms/";
+$html_path = "/var/www/html/article/";
 ini_set('display_errors', "on");
 $s = microtime(true);
 function h($str)
@@ -17,6 +19,24 @@ file_put_contents('access', 'writing');
 $datas = json_decode(mb_convert_encoding(file_get_contents('data.json'), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'), 'true');
 foreach ($datas as $data) {
     if (isset($data['tag'])) {
+        if (is_array($data['tag'])) {
+            if (stristr(implode("", $data['tag']), 'public')) {
+                $html = preg_replace('/img([0-9]{14})/i', '<img src=webp/$1.webp style="width:640px;hieght:auto;max-width:50%;">', htmlspecialchars_decode($data['memo']));
+                file_put_contents($html_path.$data['id'].".htm", $html);
+                preg_match_all('/img([0-9]{14})/i', $data['memo'], $encode_img);
+                foreach($encode_img as $img_address){
+                    if (file_exists($idms_path.'png/' . $id . '.png')) {
+                        $baseImage = imagecreatefrompng($idms_path.'png/' . $id . '.png');
+                        list($width, $hight, $type) = getimagesize($idms_path.'png/' . $id . '.png'); // 元の画像名を指定してサイズを取得
+                        $image = imagecreatetruecolor($width, $hight); // サイズを指定して新しい画像のキャンバスを作成
+                        imagecopyresampled($image, $baseImage, 0, 0, 0, 0, $width, $hight, $width, $hight); // 画像のコピーと伸縮
+                        imagewebp($image, $idms_path.'l_webp/' . $id . '.webp', 100); // コピーした画像を出力する
+                        unset($baseImage);
+                        unset($image);
+                    }
+                }
+            }
+        }
     } else {
     }
 }
