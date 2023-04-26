eeaa0546faad921e4c903f1517ec3f5495bc3eb8
shunrin824 <shunrin@shunrin.com>
2023-04-27 01:09:29 +0900

    ちょっとした変更

--- vrc_adv.php
+++ vrc_adv.php
@@ -1,29 +1,34 @@
 ﻿<?php
-function h($str) {
+function h($str)
+{
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+if(!$_GET['auth'] == '32165468795416549851'){
+    goto end;
+}
+
 ini_set('display_errors', "on");
 $date = date("YmdHis");
 $file = fopen($_FILES['log']['tmp_name'], "r");
-while($a = fgets($file)){
-    if(strpos($a, "Joining or Creating Room")){
+while ($a = fgets($file)) {
+    if (strpos($a, "Joining or Creating Room")) {
         $f[] = $a;
-    }elseif(strpos($a, "UnityEngine")){
-    }elseif(strpos($a, "OnPlayerLeftRoom")){
-    }elseif(strpos($a, "Unnamed")){
-    }elseif(strpos($a, "Activate")){
-    }elseif(strpos($a, "[Behaviour] OnPlayerJoined")){
+    } elseif (strpos($a, "UnityEngine")) {
+    } elseif (strpos($a, "OnPlayerLeftRoom")) {
+    } elseif (strpos($a, "Unnamed")) {
+    } elseif (strpos($a, "Activate")) {
+    } elseif (strpos($a, "[Behaviour] OnPlayerJoined")) {
         $f[] = $a;
-    }elseif(strpos($a, "[Behaviour] OnPlayerLeft")){
+    } elseif (strpos($a, "[Behaviour] OnPlayerLeft")) {
         $f[] = $a;
-    }elseif(strpos($a, "Attempting to resolve URL")){
+    } elseif (strpos($a, "Attempting to resolve URL")) {
         $f[] = $a;
     }
 }
 $g = implode('', $f);
 unlink($logname);
-while('1' == '1'){
-    if(!file_exists('idms/access')){
+while ('1' == '1') {
+    if (!file_exists('idms/access')) {
         break;
     }
     sleep("0.1");
@@ -38,22 +43,28 @@ $data['date'] = $date;
 $data['type'] = 'txt';
 $data['originalname'] = 'log.txt';
 $data['tag'] = array('log', 'VRChat', 'ログファイル', '解析結果');
-$data['memo'] = str_replace(array("\n","\r"), '', nl2br(h($g), false));
+$data['memo'] = str_replace(array("\n", "\r"), '', nl2br(h($g), false));
 $edata[] = $data;
-foreach($datas as $data){
+foreach ($datas as $data) {
     $edata[] = $data;
 }
-$json = json_encode($edata, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-$bytes = file_put_contents("idms/data.json", $json); 
+$json = json_encode($edata, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+$bytes = file_put_contents("idms/data.json", $json);
 unlink('idms/access');
+end:
 ?>
 <html>
- <head>
- </head>
- <body>
-  <h1><font color="red">送信が完了しました。</font></h1>
-  <p>
-  <?=(str_replace(array("\n","\r"), '', nl2br(h($g), false)))?>
-  </p>
- </body>
+
+<head>
+</head>
+
+<body>
+    <h1>
+        <font color="red">送信が完了しました。</font>
+    </h1>
+    <p>
+        <?= (str_replace(array("\n", "\r"), '', nl2br(h($g), false))) ?>
+    </p>
+</body>
+
 </html>
